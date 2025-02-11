813e0471efb967ef9400bd66ea5580db0686df6f
Evgeny Shilov <jekazza155@yandex.ru>
2025-02-11 12:01:39 +0300

    update 2

--- conftest.py
+++ conftest.py
@@ -11,8 +11,7 @@ def courier():
     Courier().courier_subsequent_deletion(courier_login["id"])
 
 @pytest.fixture()
-def courier_delete():
+def create_and_login_courier():
     courier_create = Courier().courier_registration_in_the_system_and_get_courier_data()
-    print(courier_create['data'])
-    courier_login = Courier().courier_login_in_the_system_and_get_id_courier(courier_create["data"])
-    yield courier_login
+    return courier_create['data']
+
